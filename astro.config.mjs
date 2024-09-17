858710f687c57101c1b15c3942a5454ac938f5a7
Sahil Khan <sahilkhan@Sahils-MacBook-Pro.local>
2024-09-18 02:21:05 +0530

    [FRONT-2]: Deploying to github pages: site name updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sawhil.github.io",
-  base: "gofrontend",
   integrations: [
     starlight({
       title: "Go FrontEnd",
