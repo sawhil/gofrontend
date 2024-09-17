b6465b024bd30fc1cdc8adf22a808cc86a55b999
Sahil Khan <sahilkhan@Sahils-MacBook-Pro.local>
2024-09-18 02:18:24 +0530

    [FRONT-2]: Deploying to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Go FrontEnd',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'JavaScript Questions',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Arrow Functions', slug: 'javascript/arrow' },
-					],
-				},
-			],
-		}),
-	],
+  site: "https://astronaut.github.io",
+  base: "gofrontend",
+  integrations: [
+    starlight({
+      title: "Go FrontEnd",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "JavaScript Questions",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Arrow Functions", slug: "javascript/arrow" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
